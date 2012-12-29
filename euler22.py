efced7f4bdc8d4c5b6ae44f7c2964199bedcd2e0
martin <martin@f7se.(none)>
2012-12-29 19:32:51 +0000

    solution for euler problem 22.

---
+++ euler22.py
@@ -0,0 +1,29 @@
+with open('names.txt', 'r+') as f:
+    read_data = f.read()
+read_data = sorted(read_data.split(','))
+for i in read_data:
+    i.replace('"', '')
+
+
+def ordvalue(character):
+    return ord(character) - 64
+
+def getsum(ind):
+    sum = 0
+
+    for i in read_data[ind]:
+        for j in i:
+            if ordvalue(j) >=0:
+                sum += ordvalue(j)
+           
+    return  sum * (read_data.index(read_data[ind]) + 1 )
+
+total = 0
+for i,j in enumerate(read_data):
+    total += getsum(i)
+print total
+
+        
+
+
+    
