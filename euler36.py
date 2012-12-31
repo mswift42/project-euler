126bc7ab5650f88859c74da6dcbc86a5a59789e0
martin <martin@f7se.(none)>
2012-12-31 18:39:41 +0000

    solution for euler 36.

---
+++ euler36.py
@@ -0,0 +1,23 @@
+"""Double-base palindromes
+Problem 36
+31 January 2003
+
+The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)"""
+
+def check_bin(number):
+    num = str(bin(number))[2:]
+    return num == num[::-1] 
+
+def check_int(number):
+    num = str(number)
+    return num == num[::-1]
+
+sum = 0
+for i in range(1,1000000):
+    if check_int(i) and check_bin(i):
+        sum +=i
+print sum
